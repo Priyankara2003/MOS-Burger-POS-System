9c50530e3628fae637ac53ef983781e6f677a70a
Priyankara2003 <srinathpriyankara77@gmail.com>
2024-08-06 11:37:22 +0530

    sending item to cart and increase quantity

--- app.js
+++ app.js
@@ -37,126 +37,263 @@ document.addEventListener("DOMContentLoaded", function (event) {
 });
 
 // script.js
-document.addEventListener("DOMContentLoaded", () => {
-  const decrementBtn = document.getElementById("decrement-btn");
-  const incrementBtn = document.getElementById("increment-btn");
-  const quantityInput = document.getElementById("quantity-input");
+// document.addEventListener("DOMContentLoaded", () => {
+//   const decrementBtn = document.getElementById("decrement-btn");
+//   const incrementBtn = document.getElementById("increment-btn");
+//   const quantityInput = document.getElementById("quantity-input");
 
-  decrementBtn.addEventListener("click", () => {
-    let currentValue = parseInt(quantityInput.value);
-    if (currentValue > 1) {
-      quantityInput.value = currentValue - 1;
-    }
-  });
+//   decrementBtn.addEventListener("click", () => {
+//     let currentValue = parseInt(quantityInput.value);
+//     if (currentValue > 1) {
+//       quantityInput.value = currentValue - 1;
+//     }
+//   });
 
-  incrementBtn.addEventListener("click", () => {
-    let currentValue = parseInt(quantityInput.value);
-    quantityInput.value = currentValue + 1;
-  });
-});
+//   incrementBtn.addEventListener("click", () => {
+//     let currentValue = parseInt(quantityInput.value);
+//     quantityInput.value = currentValue + 1;
+//   });
+// });
 
-//item array
+//------------------item array---------------------
 const itemList = [
   {
-    id:"B1001",
-    img:"img/burger-1.png",
-    title:"Classic Burger (Large)",
-    price:1500,
+    id: "B1001",
+    img: "img/burger-1.png",
+    title: "Classic Burger (Large)",
+    price: 1500,
     discount: 15,
-    category:"Burgers"
+    category: "Burgers",
   },
   {
-    id:"B1002",
-    img:"img/burger-1.png",
-    title:"Classic Burger (Regular)",
-    price:750,
+    id: "B1002",
+    img: "img/burger-1.png",
+    title: "Classic Burger (Regular)",
+    price: 750,
     discount: 0,
-    category:"Burgers"
+    category: "Burgers",
   },
   {
-    id:"B1003",
-    img:"img/burger-1.png",
-    title:"Turkey Burger",
-    price:1600,
+    id: "B1003",
+    img: "img/burger-1.png",
+    title: "Turkey Burger",
+    price: 1600,
     discount: 0,
-    category:"Burgers",
+    category: "Burgers",
   },
   {
-    id:"B1004",
-    img:"img/burger-1.png",
-    title:"Chicken Burger (Large)",
-    price:1400,
+    id: "B1004",
+    img: "img/burger-1.png",
+    title: "Chicken Burger (Large)",
+    price: 1400,
     discount: 0,
-    category:"Burgers",
+    category: "Burgers",
   },
   {
-    id:"B1005",
-    img:"img/burger-1.png",
-    title:"Chicken Burger (Regular)",
-    price:800,
+    id: "B1005",
+    img: "img/burger-1.png",
+    title: "Chicken Burger (Regular)",
+    price: 800,
     discount: 20,
-    category:"Burgers",
+    category: "Burgers",
   },
   {
-    id:"B1006",
-    img:"img/burger-1.png",
-    title:"Cheese Burger (Large)",
-    price:1000,
+    id: "B1006",
+    img: "img/burger-1.png",
+    title: "Cheese Burger (Large)",
+    price: 1000,
     discount: 0,
-    category:"Burgers",
+    category: "Burgers",
   },
   {
-    id:"B1007",
-    img:"img/burger-1.png",
-    title:"Cheese Burger (Regular)",
-    price:600,
+    id: "B1007",
+    img: "img/burger-1.png",
+    title: "Cheese Burger (Regular)",
+    price: 600,
     discount: 0,
-    category:"Burgers",
+    category: "Burgers",
   },
   {
-    id:"B1008",
-    img:"img/burger-1.png",
-    title:"Bacon Burger",
-    price:650,
+    id: "B1008",
+    img: "img/burger-1.png",
+    title: "Bacon Burger",
+    price: 650,
     discount: 15,
-    category:"Burgers",
+    category: "Burgers",
   },
   {
-    id:"B1009",
-    img:"img/burger-1.png",
-    title:"Shawarma Burger",
-    price:800,
+    id: "B1009",
+    img: "img/burger-1.png",
+    title: "Shawarma Burger",
+    price: 800,
     discount: 0,
-    category:"Burgers",
+    category: "Burgers",
   },
   {
-    id:"B1010",
-    img:'img/burger-1.png',
-    title:"Olive Burger",
-    price:1800,
+    id: "B1010",
+    img: "img/burger-1.png",
+    title: "Olive Burger",
+    price: 1800,
     discount: 0,
-    category:"Burgers",
+    category: "Burgers",
+  },
+  {
+    id: "test",
+    img: "img/burger-3.png",
+    title: "test Burger",
+    price: 1800,
+    discount: 0,
+    category: "Submarines",
+  },
+  {
+    id: "test2",
+    img: "img/burger-3.png",
+    title: "test3 Burger",
+    price: 1800,
+    discount: 0,
+    category: "Submarines",
+  },
+  {
+    id: "test",
+    img: "img/burger-1.png",
+    title: "test2 Burger",
+    price: 1800,
+    discount: 0,
+    category: "Pasta",
+  },
+];
+
+//-----------------Add Item cards--------------------------
+
+itemList.forEach((element) => {
+  const all = document.getElementById("all");
+  const itemCard = document.createElement("div");
+  itemCard.classList.add("item-card");
+  itemCard.dataset.id = element.id;
+  itemCard.innerHTML = `
+                <div class="img">
+                    <img src="${element.img}" alt="Product Image">
+                </div>
+                <div class="content">
+                    <h3>${element.title}</h3>
+                    <span class="price">Rs.${element.price}</span>
+                    <span class="discount">${element.discount}% off</span>
+                </div>
+            `;
+  itemCard.addEventListener("click", () => addToCart(element.id));
+  all.appendChild(itemCard);
+});
+
+itemList.forEach((element) => {
+  if (element.category == "Burgers") {
+    const burgers = document.getElementById("burgers");
+    const itemCard = document.createElement("div");
+    itemCard.classList.add("item-card");
+    itemCard.dataset.id = element.id;
+    itemCard.innerHTML = `
+                <div class="img">
+                    <img src="${element.img}" alt="Product Image">
+                </div>
+                <div class="content">
+                    <h3>${element.title}</h3>
+                    <span class="price">Rs.${element.price}</span>
+                    <span class="discount">${element.discount}% off</span>
+                </div>
+            `;
+    itemCard.addEventListener("click", () => addToCart(element.id));
+    burgers.appendChild(itemCard);
+
+  } else if (element.category == "Submarines") {
+    const submarines = document.getElementById("submarines");
+    const itemCard = document.createElement("div");
+    itemCard.classList.add("item-card");
+    itemCard.dataset.id = element.id;
+    itemCard.innerHTML = `
+                <div class="img">
+                    <img src="${element.img}" alt="Product Image">
+                </div>
+                <div class="content">
+                    <h3>${element.title}</h3>
+                    <span class="price">Rs.${element.price}</span>
+                    <span class="discount">${element.discount}% off</span>
+                </div>
+            `;
+    itemCard.addEventListener("click", () => addToCart(element.id));
+    submarines.appendChild(itemCard);
   }
-]
+});
 
-let cardBody = document.getElementById("all");
+//-------------------------add to cart function------------------
 
-let card = "";
+let cartItem = document.getElementById("all");
+let cart = [];
 
-itemList.forEach(element => {
-    card += `<div class="item-card">
-    <div class="img">
-      <img src=${element.img}></img>
-    </div>
-    <div class="content">
-      <h3>${element.title}</h3>
-      <span class="price">Rs.${element.price}</span>
-      <span class="discount">${element.discount}% off</span>
-    </div>
-  </div>
-  `
-})
+function addToCart(product_Id) {
+  let itemIsInTheCart = cart.findIndex((value) => value.productId === product_Id);
 
-cardBody.innerHTML = card;
+  if (cart.length == 0) {
+    cart=[{
+      productId: product_Id,
+      quantity: 1
+    }]
+  }else if (itemIsInTheCart < 0) {
+    cart.push({
+      productId: product_Id,
+      quantity: 1
+    })
+  }else{
+    cart[itemIsInTheCart].quantity = cart[itemIsInTheCart].quantity+1;
+  }
+  addToHtml();
+}
 
-console.log(card);
+const addToHtml = () => {
+  let cartBody = document.getElementById("card-body");
+  clearContentAfterOrderId();
+  
+  cart.forEach(carts => {
+    const items = document.createElement("div");
+    items.setAttribute("id", "items");
+    items.classList.add("cart-item", "d-flex", "gap-4", "mt-3", "mb-3", "align-items-center", "justify-content-center");
+    items.dataset.id = carts.productId;
+    let indexOfObject = itemList.findIndex((value) => value.id == carts.productId);
+    let objectInfo = itemList[indexOfObject];
+    
+    items.innerHTML = `
+      <div class="desc-item">
+        <h5>${objectInfo.title}</h5>
+        <span>${objectInfo.discount}% off</span>
+      </div>
+      <div class="quantity-container">
+        <button class="quantity-btn" id="decrement-btn">-</button>
+        <input type="text" id="quantity-input" value="${carts.quantity}" readonly />
+        <button class="quantity-btn" id="increment-btn">+</button>
+      </div>
+      <div class="price d-flex justify-content-center">
+        <h6>Rs.${objectInfo.price * carts.quantity}</h6>
+      </div>
+      <div class="trash-icon">
+        <i class="bx bxs-trash avatar" role="button"></i>
+      </div>
+    `;
+    
+    cartBody.appendChild(items); // Append each items element inside the loop
+  });
+}
+
+function clearContentAfterOrderId() {
+
+  const parent = document.querySelector('.card-body');
+
+  const orderIdElement = parent.querySelector('.order-id');
+
+  let sibling = orderIdElement.nextElementSibling;
+
+  while (sibling) {
+      let nextSibling = sibling.nextElementSibling;
+      sibling.remove();
+      sibling = nextSibling;
+  }
+}
+
+//------------------increase and decrease quantity-----
